f774e7b05a48e93dc4136732835c1414b6e3d9f3
meckblack <meckydrix@gmail.com>
2017-09-28 23:22:52 +0100

    Moved the entities to lizon.Data.Objects

--- Lizon.Data.DbConnections/DataContext/lizon_DB.cs
+++ Lizon.Data.DbConnections/DataContext/lizon_DB.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using Lizon.Data.Objects.Entities;
+using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace Lizon.Data.DbConnections.DataContext
--- Lizon.Data.Objects/Entities/Role.cs
+++ Lizon.Data.Objects/Entities/Role.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Lizon.Models
+namespace Lizon.Data.Objects.Entities
 {
     public class Role
     {
